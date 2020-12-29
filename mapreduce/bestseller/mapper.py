45be2d43db750a29c23f2a078822ef04785c4990
n3m1-sys <mromera99@gmail.com>
2020-12-29 13:34:45 +0100

    Fixed mapper.py

--- mapreduce/bestseller/mapper.py
+++ mapreduce/bestseller/mapper.py
@@ -11,33 +11,20 @@ import sys
 import csv
 
 first_line = True
-title = -1
-rank = -1
+title = 0
+rank = 2
 bestrank = -1
 bestranktitle = ""
 
 data = sys.stdin.readlines()
 
 for line in csv.reader(data, delimiter=';'):
-    if first_line:
-        # Buscamos en que columna se encuentran los datos de title y bestseller-rank
-        column_count = 0
-        for column in line:
-            if column == "title":
-                title = column_count
-            if column == "bestsellers-rank":
-                rank = column_count
-            column_count += 1
-        if title == -1 or rank == -1:
-            raise Exception("title or rank column not found")
-        first_line = False
-    else:
-        if line[rank] != "":
-            # El libro tiene ranking
-            thisrank = int(line[rank].replace('.','').replace(',',''))
-            if thisrank < bestrank or bestrank == -1 :
-                # Ha encontrado un libro con mejor ranking, o todavia no tenia un libro con mejor ranking
-                bestrank = thisrank
-                bestranktitle = line[title]
+    if line[rank] != "" and line[title] != "title" and line[rank] != "bestsellers-rank":
+        # El libro tiene ranking
+        thisrank = int(line[rank].replace('.','').replace(',',''))
+        if thisrank < bestrank or bestrank == -1 :
+            # Ha encontrado un libro con mejor ranking, o todavia no tenia un libro con mejor ranking
+            bestrank = thisrank
+            bestranktitle = line[title]
 
 print("%s\t%s" % (str(bestrank), bestranktitle))
